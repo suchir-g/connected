41f28bdd150abd2d4aee0f86211e36e58662e0c1
dunkshotenthusiast <18sgupta@heckgrammar.co.uk>
2025-01-28 22:16:54 +0000

    dfs

--- frontend/src/pages/review/ReviewGame.js
+++ frontend/src/pages/review/ReviewGame.js
@@ -10,7 +10,7 @@ import Loading from "../../components/loading/Loading";
 const ReviewGame = () => {
   const { gameId, playerId } = useParams();
   const { darkMode } = useTheme(); 
-
+//d
   const [board, setBoard] = useState(Array(6).fill(Array(7).fill(0)));
   const [moves, setMoves] = useState([]);
   const [gameMode, setGameMode] = useState("connect-4");
