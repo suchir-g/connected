1e687d89ca485d36dbec527c34ec43e326b610bf
suchir-g <18sgupta@heckgrammar.co.uk>
2025-01-31 12:04:43 +0000

    dsffdsss

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://192.168.35.37:5000" || "http://127.0.0.1:5000";
+const API_BASE = "http://192.168.35.37:5000";
 
 // request wrapper (handles errors pretty much)
 const handleRequest = async (requestFn) => {
@@ -8,6 +8,7 @@ const handleRequest = async (requestFn) => {
     const response = await requestFn();
     return response;
   } catch (error) {
+    console.log("err: ", error)
     throw error;
   }
 };
